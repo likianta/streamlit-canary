a30a92de745088918890c71dececc6d52d9062cc
likianta <likianta@foxmail.com>
2024-12-12 16:22:57 +0800

    add card component

--- streamlit_canary/components/card.py
+++ streamlit_canary/components/card.py
@@ -1,5 +1,15 @@
+import typing as t
+from contextlib import contextmanager
 from functools import partial
 
 import streamlit as st
 
-card = bordered_container = partial(st.container, border=True)
+bordered_container = partial(st.container, border=True)
+
+
+@contextmanager
+def card(label: str = None) -> t.ContextManager:
+    with st.container(border=True):
+        if label:
+            st.write(':blue[**{}**]'.format(label))
+        yield
