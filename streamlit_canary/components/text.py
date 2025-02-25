b7d5ac0cc98023c6455848a479a804c909c02812
Likianta <likianta@foxmail.com>
2025-02-25 15:49:18 +0800

    tweaks

--- streamlit_canary/components/text.py
+++ streamlit_canary/components/text.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 
 def hint(text: str) -> None:
+    # assert '\n' not in text
+    # st.write(':gray[{}]'.format(text.replace('[', '\\[')))
     st.markdown(
         f'<div style="color: gray;">{text}</div>',
         unsafe_allow_html=True
