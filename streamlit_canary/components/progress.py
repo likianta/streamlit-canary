3dd6c4975aabfa0ddb59e3314d9efb5c487f3f55
Likianta <likianta@foxmail.com>
2026-06-03 10:15:09 +0800

    `Progress` supports `with` statement

--- streamlit_canary/components/progress.py
+++ streamlit_canary/components/progress.py
@@ -8,6 +8,7 @@ example:
             prog.update(sheet.title)
             ...
 """
+
 import streamlit as st
 import typing as tp
 from contextlib import contextmanager
@@ -20,20 +21,28 @@ def progress(
     total: int = 0,
     auto_close: bool = True,
 ) -> tp.Generator['Progress', None, None]:
-    prog = Progress(label, total)
-    yield prog
-    if auto_close:
-        sleep(0.5)
-        prog.close()
+    with Progress(label, total, auto_close) as prog:
+        yield prog
 
 
 class Progress:
-    def __init__(self, label: str, total: int = 0):
+    def __init__(
+        self, label: str, total: int = 0, auto_close: bool = True
+    ) -> None:
         self._prog = st.progress(0, label)
         self.total = total
         self.index = 0
-    
-    def update(self, text: str = ''):
+        self._auto_close = auto_close
+
+    def __enter__(self) -> 'Progress':
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        if self._auto_close:
+            sleep(0.2)
+            self.close()
+
+    def update(self, text: str = '') -> None:
         self.index += 1
         if text:
             self._prog.progress(
@@ -45,6 +54,6 @@ class Progress:
                 self.index / self.total,
                 '{:.2%}'.format(self.index / self.total),
             )
-    
-    def close(self):
+
+    def close(self) -> None:
         self._prog.empty()
