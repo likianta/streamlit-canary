6a6cbece67d4a489153d63ed8c4039a903bd975c
Likianta <likianta@foxmail.com>
2026-06-22 17:18:09 +0800

    change columns use style

--- streamlit_canary/components/column.py
+++ streamlit_canary/components/column.py
@@ -1,29 +1,38 @@
 import typing as t
+from contextlib import contextmanager
 
 import streamlit as st
-
-from ._typing import AnyContainer
 
 column = st.container
 
 
-def columns(count: t.Union[int, t.Tuple[int, ...]], **kwargs) -> '_Columns':
-    return _Columns(count, **kwargs)
+@contextmanager
+def columns(
+    segment: t.Union[int, t.Tuple[t.Union[int, float], ...]], **kwargs
+) -> t.Generator[t.List[t.Any], None, None]:
+    """
+    usage:
+        with columns((7, 3)) as cols:
+            host = cols[0].text_input('Host')
+            port = cols[1].number_input('Port')
+    """
+    cols = st.columns(segment, **kwargs)
+    yield cols
 
 
-class _Columns:
-    def __init__(
-        self, count: t.Union[int, t.Tuple[int, ...]], **kwargs
-    ) -> None:
-        self._cols = st.columns(count, **kwargs)
-        self._idx = -1
-        self._length = count if isinstance(count, int) else len(count)
-    
-    def __getitem__(self, item: int) -> AnyContainer:
-        return self._cols[item]
-    
-    def next(self) -> AnyContainer:
-        self._idx += 1
-        if self._idx >= self._length:
-            self._idx = 0
-        return self._cols[self._idx]
+# class _Columns:
+#     def __init__(
+#         self, count: t.Union[int, t.Tuple[int, ...]], **kwargs
+#     ) -> None:
+#         self._cols = st.columns(count, **kwargs)
+#         self._idx = -1
+#         self._length = count if isinstance(count, int) else len(count)
+
+#     def __getitem__(self, item: int) -> AnyContainer:
+#         return self._cols[item]
+
+#     def next(self) -> AnyContainer:
+#         self._idx += 1
+#         if self._idx >= self._length:
+#             self._idx = 0
+#         return self._cols[self._idx]
